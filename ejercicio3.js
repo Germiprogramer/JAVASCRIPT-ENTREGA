ce61912d2ac659764fcc4c3a44ca1c550f749f3a
Germiprogramer <llorentemunozgerman@gmail.com>
2023-02-17 19:50:26 +0100

    commit

--- ejercicio3.js
+++ ejercicio3.js
@@ -1,10 +1,17 @@
 function comprobarArmstrong(numero) {
-    var numeroString = numero.toString();
     var suma = 0;
-    for (var i = 0; i < numeroString.length; i++) {
-        suma += Math.pow(numeroString[i], numeroString.length);
+    for (var i = 0; i < numero; i++) {
+        for (var j = 0; j < numero; j++) {
+            for (var k = 0; k < numero; k++) {
+                var suma = i**3 + j**3 + k**3;
+                if (suma == numero) {
+                    return true;
+                }
+            }
+        }
+    
     }
-    return suma==numero;
+    return false;
 }
 
 function hallararmstrong() {
