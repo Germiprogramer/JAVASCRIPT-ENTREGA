5920ba0a4841990498cd4ee739e0f75c80c510ca
Germiprogramer <llorentemunozgerman@gmail.com>
2023-02-17 11:56:25 +0100

    a

--- ejercicio1.js
+++ ejercicio1.js
@@ -2,7 +2,7 @@ function imprimirnumeros() {
     var i = 0;
     for (i; i<=100; i++) {
         if (i%3==0 && i%5!=0) {
-            document.write(i + "es divisible entre 3" +  "<br>");
+            document.write(i + " es divisible entre 3" +  "<br>");
         }
         else if (i%5==0 && i%3!=0) {
             document.write(i + "es divisible entre 5" + "<br>");
--- ejercicio3.js
+++ ejercicio3.js
@@ -1,6 +1,18 @@
-function armstrong(numero) {
-    var comparacion; 
-
-    while 
-
+function comprobarArmstrong(numero) {
+    var numeroString = numero.toString();
+    var suma = 0;
+    for (var i = 0; i < numeroString.length; i++) {
+        suma += Math.pow(numeroString[i], numeroString.length);
+    }
+    return suma==numero;
 }
+
+function hallararmstrong() {
+    for (var i = 1; 100 <= i <= 1000; i++) {
+        if (comprobarArmstrong(i) == true) {
+            document.write(i + " es un numero de Armstrong" + "<br>");
+        }
+    }
+}
+
+hallararmstrong();
